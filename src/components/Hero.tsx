e7b5c907f54f6a24e9b7b408e3040d5526c3cacb
Aurelio-One <maurici.aurelio@gmail.com>
2025-06-09 20:59:04 +0200

    Push all current changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export function Hero() {
       <div className="relative container mx-auto px-4 py-24">
         <div className="flex flex-col items-center text-center space-y-6">
           <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
-            Ma préférence à moi
+            PRÉFÉRENCE
           </h1>
           <p className="max-w-[700px] text-white-500 md:text-xl dark:text-gray-400">
             Réservez un droit de priorité sur un bien, avant même qu’il soit mis en vente.  
